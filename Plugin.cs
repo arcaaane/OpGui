75ac178214a189a11c97909dce9dbf10dca5dbec
microsoft <tuckeralbert.le.e@gmail.com>
2025-02-15 10:10:21 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
 using BepInEx;
 
-namespace OpGui
+namespace OptimizeX
 {
-    [BepInPlugin("microsoft.op.ui", "microsoft.op.ui", "1.0.1")]
+    [BepInPlugin("microsoft.optimize.x", "microsoft.optimize.x", "1.0.1")]
     public class GUI : BaseUnityPlugin
     {
+        public static void TextureLimit()
+        {
+            QualitySettings.globalTextureMipmapLimit = 99999;
+        }
+
+        public static void DisableTextureLimit()
+        {
+            QualitySettings.globalTextureMipmapLimit = 1;
+        }
+
+        public static void LowerFps()
+        {
+            Application.targetFrameRate = 30;
+        }
+        
         UnityEngine.GUI.color = Color.black;
         if (GUI.Button(new Rect(0f, 0f, 280f, 35f), "Lag All"))
         {
