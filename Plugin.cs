387c1fb62cd02449af1522d8b8e0844a04567085
microsoft <tuckeralbert.le.e@gmail.com>
2025-02-15 09:18:13 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -3,9 +3,13 @@ using BepInEx;
 
 namespace OpGui
 {
-    [BepInPlugin("Your.Name.Here", "Your.Name.Here", "1.0.0")]
+    [BepInPlugin("microsoft.op.ui", "microsoft.op.ui", "1.0.1")]
     public class GUI : BaseUnityPlugin
     {
         UnityEngine.GUI.color = Color.black;
+        if (GUI.Button(new Rect(0f, 0f, 280f, 35f), "Lag All"))
+        {
+            Debug.Log("Button clicked!"); 
+        }
     }
 }
